8df9ecfba827c106a014f1a322e26967c5290639
Dylan Jeffers <sapientech@openmailbox.org>
2016-09-30 03:18:28 -0700

    Clean p027.scm Add foof-loop

--- solved/p027.scm
+++ solved/p027.scm
@@ -1,16 +1,20 @@
 ;; Quadratic primes
-(define-module (solved p027))
+(define-module (euler solved p027)
+  #:export (p027-solve))
 
-(use-modules (euler prmes))
+(use-modules (euler primes)
+             (wak foof-loop))
+
+(define (p027-solve)
+  (max-consecutive-primes 1000 1001))
 
 (define (quadratic n a b)
   (+ (* n n) (* a n) b))
 
 (define (consecutive-primes a b)
-  (let lp ([n 0])
-    (if (prime? (quadratic n a b))
-	(lp (1+ n))
-	n)))
+  (loop ((for n (up-from 0))
+         (while (prime? (quadratic n a b))))
+        => n))
 
 (define* (max-consecutive-primes a-range #:optional (b-range a-range))
   (let lp ([a (* -1 a-range)] [b (* -1 b-range)] [max-ab (list 0 0 0)])
