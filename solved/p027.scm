60d1c78b8d12d8457aa974727572700e4f4c4570
Dylan Jeffers <sapientech@openmailbox.org>
2017-02-24 15:07:30 -0800

    Use arguile

--- solved/p027.scm
+++ solved/p027.scm
@@ -1,45 +1,35 @@
 ;; Quadratic primes
-(define-module (euler solved p027))
-(use-modules (srfi srfi-1)
-             (euler primes)
-             (euler utils)
-             (wak foof-loop)
-             (wak foof-loop nested))
+(module (euler solved p027))
+(use (arguile)
+     (arguile guile)
+     (euler primes)
+     (euler utils)
+     ((srfi srfi-1) #:select (reduce)))
 
-(define-public (p027-solve)
-  (display-quadratic
-   (quadratic-w/most-consecutive-primes 1000 1001)))
+(def p027-solve (a-max :o (b-max :as a-max))
+  (->> (reduce >primes
+               `((quad 0 0 0) 0)
+               (map calc-primes (quads a-max b-max)))
+       prn-quad))
 
-(define (display-quadratic quadratic)
-  (format #f "n^2 + ~sn + ~s has ~s quadratic prime(s)"
-          (second quadratic)
-          (third quadratic)
-          (first quadratic)))
+(data quad (a b c)
+      #:app (fn (n)
+              (+ (* n n) (* (quad-a self) n) (quad-b self))))
 
-(define* (quadratic-w/most-consecutive-primes a-max
-                                           #:optional (b-max a-max))
-  (reduce keep-quadratic-with>consecutive-primes
-          min-quadratic
-          (collect-consecutive-quadratic-primes a-max b-max)))
+(def prn-quad ((quad primes))
+  (apply format #f "~sn^2 + ~sn + ~s has ~s quadratic prime(s)"
+         `(,@(map (\\ _ quad) `(,quad-a ,quad-b ,quad-c))
+           ,primes)))
 
-(define (keep-quadratic-with>consecutive-primes quad1 quad2)
-  (let ((prime-count1 (car quad1))
-        (prime-count2 (car quad2)))
-    (if (> prime-count1 prime-count2) quad1 quad2)))
+(def >primes ((q1 p1) (q2 p2))
+  (if (> p1 p2) `(,q1 ,p1) `(,q2 ,p2)))
 
-(define min-quadratic '(0 0 0))
-
-;;; TODO: could have a better name, if not use a comment
-(define (collect-consecutive-quadratic-primes a-max b-max)
+(def quads (a-max b-max)
   (collect-list (for a (up-from (- a-max) (to a-max)))
                 (for b (up-from (- b-max) (to b-max)))
-                (list (consecutive-quadratic-primes a b)
-                      a b)))
+                (quad 1 a b)))
 
-(define (consecutive-quadratic-primes a b)
+(def calc-primes ((($ <quad> a b) :as quad))
   (loop ((for n (up-from 0))
-         (while (prime? (quadratic n a b))))
-        => n))
-
-(define (quadratic n a b)
-  (+ (* n n) (* a n) b))
+         (while (prime? (quad n))))
+      => `(,quad ,n)))
