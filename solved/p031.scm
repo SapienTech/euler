da196652317e5a5efff765293585673650cf944b
Dylan Jeffers <sapientech@openmailbox.org>
2017-02-26 20:31:24 -0800

    p031: Use arguile

--- solved/p031.scm
+++ solved/p031.scm
@@ -1,56 +1,12 @@
-;; Coin sums (using dynamic programming)
-;; Both top-down and bottom-up approaches presented (bottom-up significantly faster
+(module (euler solved p031))
+(use (arguile))
 
-(define-module (euler solved p031))
-(use-modules (srfi srfi-1)
-             (wak foof-loop)
-             (ice-9 match))
+(def coins '(200 100 50 20 10 5 2 1))
 
-(define coins '(200 100 50 20 10 5 2 1))
-
-;;; Top-down:
-
-(define* (coin-sums amount #:optional (coins coins))
-  (if (<= (length coins) 1) 1
-      (loop continue ((amount amount))
-            (if (< amount 0) 0
-                (+ (continue (- amount (car coins)))
-                   (coin-sums amount (cdr coins)))))))
-
-(define* (coin-sums-match amount #:optional (coins coins))
-  (match coins
-    ((coin) 1) 
-    ((coin . rest)
-     (loop continue ((amount amount))
-           (if (<= amount 0) 1
-               (+ (continue (- amount coin))
-                  (coin-sums amount rest)))))))
-
-;;; TODO: simplify proc
-(define* (top-down-w/memoization amount #:optional (coins coins))
-  (define diff-coins-count (length coins))
-  (define cache (make-array #f (1+ amount) (1+ diff-coins-count)))
-  (let lp ([curr-amount amount] [coins coins])
-    (if (<= (length coins) 1) 1
-	(let in-lp ([curr-amount curr-amount] [combinations 0])
-	  (if (< curr-amount 0) combinations
-	      (in-lp
-	       (- curr-amount (car coins))
-	       (let* ([cached-val
-		       (array-ref cache
-				  curr-amount
-				  (- diff-coins-count (length coins)))]
-			[sub-combinations
-			 (if cached-val cached-val
-			     (lp curr-amount (cdr coins)))])
-		 (array-set! cache
-			     (+ combinations sub-combinations) curr-amount
-			     (- diff-coins-count (length coins)))
-		 (+ combinations sub-combinations)))))))
-  (array-ref cache amount (car coins)))
-
-;; Bottom-up
-;; TODO: finish
-(define (coin-combinations-for-amount-bottom-up amount coins)
-  (define cache (make-vector (1+ amount) #f))
-  (vector-set! cache 0 1))
+(def coin-sums (amount :o ((coin . stack) :or coins))
+  (if (nil? stack) 1
+      (loop rec ((amount amount))
+        (if (< amount 0) 0
+            (+ (rec (- amount coin))
+               (coin-sums amount stack))))))
+;;; TODO: add memoization alg
