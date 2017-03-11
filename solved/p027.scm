7df9af6cd93b44365e10a02a3a0a2c982d34d71a
Dylan Jeffers <sapientech@openmailbox.org>
2017-03-10 23:16:15 -0800

    p027 final arguile impl

--- solved/p027.scm
+++ solved/p027.scm
@@ -1,12 +1,11 @@
 ;; Quadratic primes
 (module (euler solved p027))
 (use (arguile)
-     (arguile guile)
      (euler primes)
      (euler utils)
-     ((srfi srfi-1) #:select (reduce)))
+     ((srfi srfi-1) :select (reduce)))
 
-(def p027-solve (a-max :o (b-max :as a-max))
+(def p027-solve (a-max :o (b-max :or a-max))
   (->> (reduce >primes
                `((quad 0 0 0) 0)
                (map calc-primes (quads a-max b-max)))
@@ -14,22 +13,25 @@
 
 (data quad (a b c)
       #:app (fn (n)
-              (+ (* n n) (* (quad-a self) n) (quad-b self))))
+              (+ (* n n)
+                 (* (quad-a self) n)
+                 (quad-b self))))
 
 (def prn-quad ((quad primes))
-  (apply format #f "~sn^2 + ~sn + ~s has ~s quadratic prime(s)"
-         `(,@(map (\\ _ quad) `(,quad-a ,quad-b ,quad-c))
+  (app format #f "~sn^2 + ~sn + ~s has ~s quadratic prime(s)"
+         `(,@(map (\\ _ quad)
+                  `(,quad-a ,quad-b ,quad-c))
            ,primes)))
 
-(def >primes ((q1 p1) (q2 p2))
-  (if (> p1 p2) `(,q1 ,p1) `(,q2 ,p2)))
+(def >primes (((_ p1) :as a) ((_ p2) :as b))
+  (if (> p1 p2) a b))
 
 (def quads (a-max b-max)
   (collect-list (for a (up-from (- a-max) (to a-max)))
                 (for b (up-from (- b-max) (to b-max)))
                 (quad 1 a b)))
 
-(def calc-primes ((($ <quad> a b) :as quad))
+(def calc-primes (quad)
   (loop ((for n (up-from 0))
          (while (prime? (quad n))))
       => `(,quad ,n)))
