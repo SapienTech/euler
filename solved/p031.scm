e9b03ac2a83736756ef285999e635dff9049c8cb
Dylan Jeffers <sapientech@openmailbox.org>
2017-11-14 20:07:30 -0800

    p031 add argyle
    Note: coin-sums1 is what i think argyle should eventually be able to
    handle

--- solved/p031.scm
+++ solved/p031.scm
@@ -1,12 +1,20 @@
-(module (euler solved p031))
+(ns (euler solved p031))
 (use (argyle))
 
 (def coins '(200 100 50 20 10 5 2 1))
 
-(def coin-sums (amount :o ((coin . stack) :or coins))
-  (if (nil? stack) 1
+;;; This is how we want to write it
+(def coin-sums1
+  (fn-case
+   ((amount (coin)) 1)
+   ((amount :o ((coin . rest) :or coins))
+    (loop rec ((amount amount))
+          (+ (rec (- amount coin))
+             (coin-sums amount rest))))))
+
+(def coin-sums2 (amount :o ((coin . rest) :or coins))
+  (if (nil? rest) 1
       (loop rec ((amount amount))
-        (if (< amount 0) 0
+        (if (neg? amount) 0
             (+ (rec (- amount coin))
-               (coin-sums amount stack))))))
-;;; TODO: add memoization alg
+               (coin-sums2 amount rest))))))
