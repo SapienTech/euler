7f7640c12ca57c50c954231e7dfe59c9f9be7b6d
Dylan Jeffers <sapientech@openmailbox.org>
2017-11-16 15:04:44 -0800

    Update p027

--- solved/p027.scm
+++ solved/p027.scm
@@ -1,37 +1,43 @@
 ;; Quadratic primes
 (ns (euler solved p027))
 (use (argyle)
-     (euler primes)
+     ((euler primes) :select (prime?))
      (euler utils)
-     ((srfi srfi-1) :select (reduce)))
+     ((srfi srfi-1) :select (reduce zip)))
 
-(def p027-solve (a-max :o (b-max :or a-max))
+(def p027-solve (b-max :o (c-max :or b-max))
   (->> (reduce >primes
-               `((quad 0 0 0) 0)
-               (map calc-primes (quads a-max b-max)))
+               default-quad
+               (gen-quads b-max c-max))
        prn-quad))
 
-(data quad (a b c)
+(data quad (a b c primes)
+  :init (quad a b c)
   :app (fn (n)
-         (+ (* n n)
-            (* (quad-a self) n)
-            (quad-b self))))
+         (+ (* (quad-a self) (^ n 2))
+            (* (quad-b self) n)
+            (quad-c self))))
 
-(def prn-quad ((quad primes))
-  (app format #f "~sn^2 + ~sn + ~s has ~s quadratic prime(s)"
-         `(,@(map (\\ _ quad)
-                  `(,quad-a ,quad-b ,quad-c))
-           ,primes)))
+(def default-quad (quad 0 0 0))
 
-(def >primes (((_ p1) :as a) ((_ p2) :as b))
-  (if (> p1 p2) a b))
+(def >primes (a b)
+  (= a (calc-primes a))
+  (= b (calc-primes b))
+  (if (> (quad-primes a) (quad-primes b))
+      a b))
 
-(def quads (a-max b-max)
-  (collect-list (for a (up-from (- a-max) (to a-max)))
-                (for b (up-from (- b-max) (to b-max)))
-                (quad 1 a b)))
+(def gen-quads (b-max c-max)
+  (collect-list (for b (up-from (- b-max) (to b-max)))
+                (for c (up-from (- c-max) (to c-max)))
+                (quad 1 b c)))
 
 (def calc-primes (quad)
-  (loop ((for n (up-from 0))
-         (while (prime? (quad n))))
-      => `(,quad ,n)))
+  (if (quad-primes quad) quad
+      (loop ((for n (up-from 0))
+             (while (prime? (quad n))))
+        => (quad-primes! quad n))))
+
+(def prn-quad (quad)
+  (app format "n^2 + ~sn + ~s has ~s quadratic prime(s)"
+       (map (\\ _ quad)
+            `(,quad-b ,quad-c ,quad-primes))))
